6f1c70c95c576d161dcdfef4fd9b7e1629c6a0d7
Blueredgray <113849163+Blueredgray@users.noreply.github.com>
2022-10-30 22:08:24 +0300

    Add files via upload

---
+++ kursov_m_a/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+int main() {
+    std::array<int, 101> b = {};
+    int n = 0;
+    std::cin >> n;
+    int p = 0;
+    for (int i = 1; i <= n; i += 1) {
+        std::cin >> p;
+        b[p] = i;
+    }
+    for (int i = 1; i <= n; i += 1) {
+        std::cout << b[i] << " ";
+    }
+}
+    
+    
